7b1d25d10c83ad7230b7dcef3434c1f9311f4ddc
Christy <caburris@ucdavis.edu>
2018-03-09 12:43:37 -0800

    debugged code, quiz works

--- js/quiz.js
+++ js/quiz.js
@@ -1,8 +1,7 @@
 var questions = [
-    ['Should you pet a dog who is baring his teeth and growling? ', 'No'],
-    ['Is it okay to pull a dogs tail?', 'No'],
-    ['Can you feed a dog chocolate?', 'No'],
-    ['Should you always ask a dogs owner for permission before you pet the dog?', 'Yes']
+    ['How many letters are in the alphabet? ', 26],
+    ['How many colors are in the rainbow?', 7],
+    ['How many sides does a square have?', 4],
 ];
 var correctAnswers = 0;
 var question;
@@ -10,19 +9,41 @@ var answer;
 var answer;
 var response;
 var html;
+var correct = [];
+var wrong = [];
 
 function print(message) {
-  document.write(message);
+  var outputDiv = document.getElementById('output');
+  outputDiv.innerHTML = message;
+}
+
+function buildList(arr){
+    var listHTML = '<ol>';
+        for (var i = 0; i < arr.length; i +=1){
+            listHTML += '<li>' + arr[i] + '</li>';
+        }
+        listHTML += '</ol>';
+        return listHTML;
 }
 
 for (var i = 0; i < questions.length; i +=1  ){
     question = questions[i][0];
     answer = questions[i][1];
-    response = (prompt(question));
+    response = parseInt(prompt(question));
+    //response = parseInt(prompt(question));
+    //will transform number responses into strings
+    //I need to get it to .toUpperCase();
     if (response === answer) {
         correctAnswers += 1;
+        correct.push(question);
+    } else{
+        wrong.push(question);
     }
 }
 
 html = "You got " + correctAnswers + " question(s) right.";
+html += '<h2>You got these questions correct:<h2>';
+html +=buildList(correct);
+html += '<h2>You got these questions wrong:</h2>';
+html += buildList(wrong);
 print(html);
